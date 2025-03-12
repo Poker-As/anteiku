f94dbf5c563359c23d53fe81b7b3c92131f4100b
Poker-As <dvillalba307@gmail.com>
2025-03-12 11:12:55 -0500

    carga inicial del proyecto

---
+++ src/main/java/com/mycompany/anteiku/Anteiku.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anteiku;
+
+/**
+ *
+ * @author dvill
+ */
+public class Anteiku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
